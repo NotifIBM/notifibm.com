c70147dd86ad766fb7653d5b4effff6c8df77fee
Savindu Supun <savindusupunsathsara@gmail.com>
2023-05-14 22:05:55 +0530

    Add loading spin to GPA calculator ✨

--- app/gpa/page.js
+++ app/gpa/page.js
@@ -97,95 +97,127 @@ const Page = () => {
 
   return (
     <div className="container mx-auto my-8 relative z-10 text-center flex flex-col md:flex-row md:flex-wrap pb-12">
-      <div className="m-4 p-5 text-center mx-auto">
-        <TitleText title={<>GPA Calculator</>} />
-      </div>
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col md:flex-row gap-5 text-center my-auto mx-auto px-1 mt-2 md:mt-8"
-      >
-        <label htmlFor="program" className="font-bold text-gray-300">
-          Program:
-        </label>
-        <select
-          id="program"
-          className="p-0.5 border border-gray-400 rounded-md text-sm w-48 h-8"
-          value={program}
-          onChange={(e) => {
-            setProgram(e.target.value);
-          }}
+      <div className="flex flex-col md:flex-row md:flex-wrap">
+        <div className="m-4 p-5 text-center mx-auto">
+          <TitleText title={<>GPA Calculator</>} />
+        </div>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col md:flex-row gap-5 text-center my-auto mx-auto px-1 mt-2 md:mt-8"
         >
-          <option value="" className="text-sm">Please select Batch</option>
-          {programList && programList.map((item) => (
-            <option key={item.value} value={item.value}>
-              {item.label}
-            </option>
-          ))}
-        </select>
-
-        <label htmlFor="batch" className="font-bold  text-gray-300">
-          Batch:
-        </label>
-        <select
-          id="batch"
-          className="p-0.5 border border-gray-400 rounded-md text-sm w-48 h-8"
-          value={batch}
-          onChange={(e) => {
-            setBatch(e.target.value);
-          }}
-          disabled={dataFetch}
-        >
-          <option value="">Please select Batch</option>
-          {batchList
-            && batchList.map((Batch) => (
-              <option key={Batch.value} value={Batch.value}>
-                {Batch.name}
+          <label htmlFor="program" className="font-bold text-gray-300">
+            Program:
+          </label>
+          <select
+            id="program"
+            className="p-0.5 border rounded-md text-sm w-48 h-8 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+            value={program}
+            onChange={(e) => {
+              setProgram(e.target.value);
+            }}
+          >
+            <option value="" className="text-sm">Please select Batch</option>
+            {programList && programList.map((item) => (
+              <option key={item.value} value={item.value}>
+                {item.label}
               </option>
             ))}
-        </select>
-        {/* INDEX SELECT */}
-        <label htmlFor="index" className="font-bold  text-gray-300">
-          Index:
-        </label>
-        <select
-          id="index"
-          className="p-0.5 border border-gray-400 rounded-md text-sm w-48 h-8"
-          value={index}
-          onChange={(e) => setIndex(e.target.value)}
-          disabled={dataFetch}
-        >
-          <option value="">Please select Index</option>
-          {indexList && indexList.map((item) => (
-            <option key={item.value} value={item.value}>
-              {item.name}
-            </option>
-          ))}
-        </select>
+          </select>
 
-        <button
-          type="submit"
-          className="px-4 py-2 bg-blue-500 text-white font-bold rounded-md"
-          onClick={handleSubmit}
-        >
-          Calculate GPA
-        </button>
-      </form>
+          <label htmlFor="batch" className="font-bold  text-gray-300">
+            Batch:
+          </label>
+          <select
+            id="batch"
+            className="p-0.5 border rounded-md text-sm w-48 h-8 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+            value={batch}
+            onChange={(e) => {
+              setBatch(e.target.value);
+            }}
+            disabled={dataFetch}
+          >
+            <option value="">Please select Batch</option>
+            {batchList
+              && batchList.map((Batch) => (
+                <option key={Batch.value} value={Batch.value}>
+                  {Batch.name}
+                </option>
+              ))}
+          </select>
+          {/* INDEX SELECT */}
+          <label htmlFor="index" className="font-bold  text-gray-300">
+            Index:
+          </label>
+          <select
+            id="index"
+            className="p-0.5 border rounded-md text-sm w-48 h-8 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+            value={index}
+            onChange={(e) => setIndex(e.target.value)}
+            disabled={dataFetch}
+          >
+            <option value="">Please select Index</option>
+            {indexList && indexList.map((item) => (
+              <option key={item.value} value={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </select>
 
+          <button
+            type="submit"
+            className="px-4 mt-4 md:mt-0 mx-auto py-1 bg-blue-500 text-white font-bold rounded-md flex"
+            onClick={handleSubmit}
+            disabled={loading}
+          >
+            {/* Calculate GPA
+            {loading && (
+              <svg aria-hidden="true" role="status" className="my-auto inline w-4 h-4 ml-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+              </svg>
+            )} */}
+            {loading ? (
+              <>
+                Calculating {' '}
+                <svg aria-hidden="true" role="status" className="my-auto inline w-4 h-4 ml-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+                </svg>
+              </>
+            ) : (
+              <>
+                Calculate GPA
+              </>
+            )}
+          </button>
+        </form>
+      </div>
       {loading && (
         <>
           <br />
-          <div className="mt-8 mx-auto">
-            <p className="text-lg md:text-3xl font-bold mb-2">GPA:</p>
-            <p className="text-3xl md:text-8xl font-bold text-green-500">Loading...</p>
+          <div role="status" className="mt-8 mx-auto flex flex-col justify-center text-center">
+            <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+            </svg>
+            <span className="sr-only">Loading...</span>
           </div>
         </>
       )}
+
+      {/* <div role="status" className="mt-8 mx-auto flex flex-col justify-center text-center">
+        <svg aria-hidden="true" className="w-8 h-8 lg:w-12 lg:h-12 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+        </svg>
+        <span className="sr-only">Loading...</span>
+      </div> */}
 
       {gpa !== null && (
         <div className="w-full justify-center items-center">
           <div className="mt-8 text-center">
             <p className="text-lg md:text-3xl font-bold mb-2">GPA:</p>
-            <p className="text-3xl md:text-8xl font-bold text-green-500 mx-4 px-5">{gpa === 'NaN' ? 0.00 : gpa.toFixed(2)}</p>
+            <p className="text-3xl md:text-8xl font-bold text-white mx-4 px-5">{gpa === 'NaN' ? 0.00 : gpa.toFixed(2)}</p>
           </div>
           <div className="mt-8 text-center">
             <p className="text-xl md:text-3xl font-bold mb-2">Modules:</p>
--- pages/api/gpa/programs.js
+++ pages/api/gpa/programs.js
@@ -2,6 +2,6 @@ import { NextResponse } from 'next/server';
 import { supabase } from '../../../lib/supabaseClient';
 
 export default async function GET() {
-  const { data } = await supabase.from('programs').select('*');
+  const { data } = await supabase.from('programs').select('*').eq('status', 2);
   return NextResponse.json(data);
 }
