83a0f7215deb379434259a07c679718b43217b86
Savindu Supun <savindusupunsathsara@gmail.com>
2023-05-21 21:56:21 +0530

    Add Loading UI ✨

--- app/loading.js
+++ app/loading.js
@@ -1,4 +1,13 @@
-export default function Loading() {
-  // Or a custom loading skeleton component
-  return '...';
-}
+import Image from 'next/image';
+
+const Loading = () => (
+  <div className="flex items-center justify-center min-h-screen">
+    <Image src="/assets/notifibm-logo-white.svg"
+      alt="notifibm-logo"
+      width={150}
+      height={150}
+      className="animate-pulse bg-blend-overlay mx-auto my-auto"
+    />
+  </div>
+);
+export default Loading;
